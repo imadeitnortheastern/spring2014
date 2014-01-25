43b9d41a2fc73f4a0e2b5a6386b48aa52ac39107
iMadeIT <iMadeITNortheastern@gmail.com>
2014-01-25 16:15:16 -0500

    Just in case I break everything in the next 10 minutes

--- imadeit_website/imadeit.py
+++ imadeit_website/imadeit.py
@@ -1,25 +1,47 @@
+'''import statements
+Imports are used to source code from other libraries and files
+so that we can use their methods without explicitly including all 
+of their code
+'''
 import sqlite3, re, hashlib
 import user
 from flask import Flask, session, request, redirect, \
 render_template, url_for, g, flash
 
+#First things first, I'm creating a new Flask object and calling it app
+#It will do all the routing for the requests that come in to our IP address
 app = Flask(__name__)
 
+#Here I am updating the configuration of my Flask object
+#This won't be necessary unless you are working with a database or sessions
 app.config.update(dict(
     SECRET_KEY = 'nicodemus',
     DATABASE = 'imadeit.db',
 ))
 
+############################### DATABASE SHTUFF ###############################
+#This method will create and return a connection to the database
 def connect_db():
     db = sqlite3.connect(app.config['DATABASE'])
     db.row_factory = sqlite3.Row
     return db
 
+#The last method created a new connection to the database, but we may already
+#have one open
+#this method checks if the 'g' object has a live database connection stored
+#and then either uses that or creates one
+#A NOTE ON G: g is one of the imports up at the top. g is like a global 
+#purse than you can use to store arbitrary objects, and they are accessible
+#globally in your app 
 def get_db():
     if not hasattr(g, 'db'):
         g.db = connect_db()
     return g.db
 
+#This initializes the database from a schema file. A schema file is like
+#the blueprints for a database; it describes what tables should be created
+#what rows they have, and what types of data (like strings or numbers)
+#will go into that column
 def init_db():
     with app.app_context():
         db = get_db()
@@ -27,11 +49,22 @@ def init_db():
             db.executescript(f.read())
             db.commit()
 
+#This will close the database connection everytime the app context is 
+#popped
+#To be perfectly blunt, I don't actually know when that ever happens,
+#so I wouldn't worry about understanding it either
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'db'):
         g.db.close()
 
+###############################################################################
+############################ WEB PAGE ROUTING #################################
+###############################################################################
+
+
+
+################################## HOME ####################################
 @app.route('/')
 def redirect_to_home():
     return redirect(url_for('home'))
@@ -40,9 +73,15 @@ def redirect_to_home():
 def home():
     return render_template('home.html')
 
+################################# LOGIN #####################################
+
+#Either displays the login page or redirects to the home page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+
+    #If HTTP request that came to this webpage was a 'post' request then it 
+    #should have come from the user clicking the login button, so lets check
     if request.method == 'POST':
         db = get_db() 
 	name = request.form['login_username']
@@ -64,7 +103,8 @@ def create_account():
     if request.method == 'POST':
         db = get_db() 
 	name = request.form['create_username']
-    	pw = encrypt_password(request.form['create_pw'])
+        password = request.form['create_pw']
+    	pw = encrypt_password(password)
         email = request.form['email']
 	college = request.form['college']
         marketing = request.form['marketing']
@@ -77,7 +117,7 @@ def create_account():
             + "VALUES(?, ?, ?, ?, ?, ?, 0, 0, 0)"
             db.execute(query, [name, pw, email, college, marketing, prog])
             db.commit()
-            user.create(name, pw)
+            user.create(name, password)
             set_session_vars(name, 0)
             flash('Your account was created successfully!')
             return redirect(url_for('home'))
@@ -88,22 +128,6 @@ def create_account():
                 error = 'That user name already exists :('
     return render_template('login.html', error=error)
     
-def set_session_vars(name, port):
-    session['logged_in'] = True
-    session['name'] = name
-    session['port'] = port
-
-def valid_email(email): 
-    one_char = '[a-zA-Z0-9_]'
-    chars = '{}+'.format(one_char)
-    charsdotchars = '({}+)(\.{}+)*'.format(one_char, one_char)
-    email_re ='^' + charsdotchars + '@' + chars + '\.'+ charsdotchars + '$'
-    return re.match(email_re, email) is not None
-
-def encrypt_password(password):
-    sha = hashlib.sha224(password)
-    return sha.hexdigest()
-
 @app.route('/port_authority', methods=['GET', 'POST'])
 def port_authority():
     error = None
@@ -153,6 +177,23 @@ def login_check(url):
         return redirect(url_for('login'))
     else:
         return render_template(url)
+
+def set_session_vars(name, port):
+    session['logged_in'] = True
+    session['name'] = name
+    session['port'] = port
+
+def valid_email(email): 
+    one_char = '[a-zA-Z0-9_]'
+    chars = '{}+'.format(one_char)
+    charsdotchars = '({}+)(\.{}+)*'.format(one_char, one_char)
+    email_re ='^' + charsdotchars + '@' + chars + '\.'+ charsdotchars + '$'
+    return re.match(email_re, email) is not None
+
+def encrypt_password(password):
+    sha = hashlib.sha224(password)
+    return sha.hexdigest()
+
 
 if __name__ == '__main__':
     init_db()
--- imadeit_website/schema.sql
+++ imadeit_website/schema.sql
@@ -1,12 +1,16 @@
+
 create table if not exists users (
   user_id integer primary key autoincrement,
   username text not null,
   password text not null,
+  salt text not null,
   port integer not null,
+  partner_port integer not null,
   email text not null,
   college text not null,
+  prog_expr integer not null,
   marketing text not null
-);
+ );
 
 create table if not exists students (
   user_id integer primary key autoincrement,
