3d5bd7f774c213ebf78a2e28bbafc7c3cfe6a265
James McNamara <imadeitnortheastern@gmail.com>
2014-01-31 01:33:33 -0500

    fixed user-creation bug

--- imadeit_website/schema.sql
+++ imadeit_website/schema.sql
@@ -12,15 +12,4 @@ create table if not exists users (
   marketing text not null
  );
 
-create table if not exists students (
-  user_id integer primary key autoincrement,
-  username text not null,
-  password text not null,
-  port integer not null,
-  partner_port integer not null,
-  email text not null,
-  college text not null,
-  year integer not null,
-  prog_expr integer not null,
-  marketing text not null
-);
+
